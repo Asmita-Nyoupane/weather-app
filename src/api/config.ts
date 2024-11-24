482f7ed7b8a8ca04d109ef9ef492e1548c11fd0a
Asmita Nyoupane <ashmitaneupane3600@gmail.com>
2024-11-24 18:29:28 +0545

    chnage env name

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
     BASE_URL: "https://api.openweathermap.org/data/2.5",
     GEO_CODING: "https://api.openweathermap.org/geo/1.0",
-    API_KEY: import.meta.env.VITE_API_KEY,
+    API_KEY: import.meta.env.PRIVATE_VITE_API_KEY,
     DEFAULT_PARAMS: {
         units: "metric",
         appid: import.meta.env.VITE_API_KEY,
